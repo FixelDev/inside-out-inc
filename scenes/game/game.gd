fa1c8a3deeb70c7d94000426ce480b07e8017a6e
Fixel <zielnyfranek@gmail.com>
2024-08-04 18:35:42 +0200

    Added basic game timer to count down time for decision making

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -3,7 +3,8 @@ extends Node2D
 @export var packages_scenes: Array[PackedScene] = []
 @export var package_spawn_point: Marker2D
 
-@onready var timer_to_spawn_package = %TimerToSpawnPackage
+@onready var timer_to_spawn_package: Timer = %TimerToSpawnPackage
+@onready var time_left_progress_bar: TimeLeftProgressBar = %TimeLeftProgressBar
 
 signal emergency_mode_toggled(enabled: bool)
 
@@ -13,6 +14,9 @@ var current_package: Package
 func _ready() -> void:
 	spawn_package()
 
+func _process(delta) -> void:
+	pass
+
 
 func spawn_package() -> void:
 	var package_scene: PackedScene = packages_scenes.pick_random()
@@ -21,11 +25,12 @@ func spawn_package() -> void:
 	current_package.global_position = package_spawn_point.global_position
 	
 	print(current_package.is_evil())
+	time_left_progress_bar.start_timer(10.0)
 
 
 func _on_emergency_button_pressed():
 	emergency_mode_toggled.emit(true)
-	
+	time_left_progress_bar.start_timer(7.0)
 
 func _on_accept_button_pressed():
 	destroy_package()
@@ -46,6 +51,7 @@ func _on_emergency_module_emergency_code_checked(is_correct):
 	if not is_correct:
 		return
 	
+	time_left_progress_bar.stop_timer()
 	emergency_mode_toggled.emit(false)
 	destroy_package()
 	
---
+++ scenes/game/time_left_progress_bar.gd
@@ -0,0 +1,34 @@
+class_name TimeLeftProgressBar extends ProgressBar
+
+@onready var time_left_label = %TimeLeftLabel
+@onready var timer_time_to_left = %TimerTimeToLeft
+
+var is_timer_in_progress: bool = false
+
+
+func start_timer(seconds_left: float) -> void:
+	max_value = seconds_left
+	value = seconds_left
+	time_left_label.text = str(seconds_left)
+	timer_time_to_left.wait_time = seconds_left
+	timer_time_to_left.start()
+	is_timer_in_progress = true
+	
+
+func stop_timer() -> void:
+	timer_time_to_left.stop()
+	is_timer_in_progress = false
+	
+	
+func _process(_delta) -> void:
+	if not is_timer_in_progress:
+		return
+	
+	var snapped_value: float = snapped(timer_time_to_left.time_left, 0.1)
+	value = timer_time_to_left.time_left
+	time_left_label.text = str(snapped_value)
+
+
+func _on_timer_time_to_left_timeout():
+	is_timer_in_progress = false
+	print("TIMEOUT")
