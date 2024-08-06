9560ae522ebb11aa5b152a5effb9a0d070b9c257
Fixel <zielnyfranek@gmail.com>
2024-08-07 01:45:17 +0200

    Added transition system

--- globals/scene_manager/scene_manager.gd
+++ globals/scene_manager/scene_manager.gd
@@ -1,10 +1,23 @@
 extends Node2D
 
+@export var transition: Panel
+
 @export var game_scene: PackedScene
 @export var menu_scene: PackedScene
 @export var galactic_bulletin_scene: PackedScene
 @export var game_over_scene: PackedScene
 @export var ending_scene: PackedScene
 
+
+
 func load_scene(scene_to_load: PackedScene) -> void:
+	var tween: Tween = get_tree().create_tween()
+	tween.tween_property(transition, "size:y", 720, 1).set_trans(Tween.TRANS_CUBIC)
+	
+	await tween.finished
+	
+	
 	get_tree().change_scene_to_packed(scene_to_load)
+	
+	var tween2: Tween = get_tree().create_tween()
+	tween2.tween_property(transition, "size:y", 0, 1).set_trans(Tween.TRANS_CUBIC)
