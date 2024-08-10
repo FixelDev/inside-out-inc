5216db61e4c5cbf09092f852c0b74cd18cd4da33
Fixel <zielnyfranek@gmail.com>
2024-08-10 18:57:43 +0200

    New galactic bulletin

--- scenes/menu/menu.gd
+++ scenes/menu/menu.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var button_pressed_audio_stream = %ButtonPressedAudioStream
+@onready var continue_button = %ContinueButton
 
 func _on_continue_button_pressed():
 	button_pressed_audio_stream.play()
@@ -12,3 +13,11 @@ func _on_new_game_button_pressed():
 	DayManager.current_day_index = 0
 	Globals.coins = 0
 	SceneManager.load_scene(SceneManager.galactic_bulletin_scene)
+
+
+func _on_continue_button_mouse_entered():
+	continue_button.text = "Day " + str(DayManager.current_day.number)
+
+
+func _on_continue_button_mouse_exited():
+	continue_button.text = "Continue"
